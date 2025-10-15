d0912f1ccbd045cb92b9b513d24389ec4b03d1cc
jaishish.ranjan01 <jaishish.ranjan01@gmail.com>
2025-10-15 21:31:38 +0530

    day2

---
+++ day2/arrays/twopointer/PalindromeTP.java
@@ -0,0 +1,24 @@
+package day2.arrays.twopointer;
+
+public class PalindromeTwoPointer {
+    public static void main(String[] args) {
+        String str = "abcba";
+        System.out.println(isPalindrome(str));
+    }
+
+    public static boolean isPalindrome(String str) {
+        //if string is null, empty or contains only spaces
+        if (str == null || str.isBlank()) return true;
+
+        //apply two pointer to check the palindrome
+        int i = 0;
+        int j= str.length()-1;
+
+        while (i <= j) {
+            if(str.charAt(i) != str.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
